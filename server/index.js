0cb3de4b4f3bea2ab86379f74c438f6375f16d1b
unknown <pvenkayya2005@gmail.com>
2026-06-10 14:42:08 +0530

    Render deployment fix

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ const cors = require('cors');
 const lmsData = require('./mockLMS'); // Import the mock data
 
 const app = express();
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
