cf9428ca2909347eea59a08c98b063d4d866c800
unknown <pvenkayya2005@gmail.com>
2026-06-12 12:29:31 +0530

    Connect app to progress-verification-system-1

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,13 +5,24 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyACgISYxW3sEHjQmIMAmm2trZBxSuUt5JU",
+//   authDomain: "sih-student-hub.firebaseapp.com",
+//   projectId: "sih-student-hub",
+//   storageBucket: "sih-student-hub.firebasestorage.app",
+//   messagingSenderId: "636276235310",
+//   appId: "1:636276235310:web:d53b96c3f6778e865093bf"
+// };
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyACgISYxW3sEHjQmIMAmm2trZBxSuUt5JU",
-  authDomain: "sih-student-hub.firebaseapp.com",
-  projectId: "sih-student-hub",
-  storageBucket: "sih-student-hub.firebasestorage.app",
-  messagingSenderId: "636276235310",
-  appId: "1:636276235310:web:d53b96c3f6778e865093bf"
+  apiKey: "AIzaSyBlGnNuJI30SmxWCS83lcel2xJriZBq_jA",
+  authDomain: "progress-verification-system-1.firebaseapp.com",
+  projectId: "progress-verification-system-1",
+  storageBucket: "progress-verification-system-1.firebasestorage.app",
+  messagingSenderId: "984059567308",
+  appId: "1:984059567308:web:98295cff7f960de50d3669",
+  measurementId: "G-TK37VS6138"
 };
 
 // Initialize Firebase
--- mobile-app/firebase.js
+++ mobile-app/firebase.js
@@ -4,13 +4,24 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // PASTE YOUR FIREBASE CONFIG OBJECT HERE
+// const firebaseConfig = {
+//   apiKey: "AIzaSyACgISYxW3sEHjQmIMAmm2trZBxSuUt5JU",
+//   authDomain: "sih-student-hub.firebaseapp.com",
+//   projectId: "sih-student-hub",
+//   storageBucket: "sih-student-hub.firebasestorage.app",
+//   messagingSenderId: "636276235310",
+//   appId: "1:636276235310:web:d53b96c3f6778e865093bf"
+// };
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyACgISYxW3sEHjQmIMAmm2trZBxSuUt5JU",
-  authDomain: "sih-student-hub.firebaseapp.com",
-  projectId: "sih-student-hub",
-  storageBucket: "sih-student-hub.firebasestorage.app",
-  messagingSenderId: "636276235310",
-  appId: "1:636276235310:web:d53b96c3f6778e865093bf"
+  apiKey: "AIzaSyBlGnNuJI30SmxWCS83lcel2xJriZBq_jA",
+  authDomain: "progress-verification-system-1.firebaseapp.com",
+  projectId: "progress-verification-system-1",
+  storageBucket: "progress-verification-system-1.firebasestorage.app",
+  messagingSenderId: "984059567308",
+  appId: "1:984059567308:web:98295cff7f960de50d3669",
+  measurementId: "G-TK37VS6138"
 };
 
 const app = initializeApp(firebaseConfig);
